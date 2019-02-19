d9250ad5cb53845a1602ec558e392680d81057b5
MaxPlay <maxplayyt@gmail.com>
2019-02-19 21:45:05 +0100

    Created Solution, Projects and .gitignore for the build-tool

---
+++ build-tool/Source.Build.CMD/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Source.Build.CMD
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
